1b6e9d8b520cccae6751707285f37a76e0e473aa
Dmetrios <diman23420@yandex.ru>
2022-09-08 20:56:07 +0400

    handling two similar chars

--- lib/Helper.java
+++ lib/Helper.java
@@ -1,4 +1,9 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Helper {
     // function which biuld list of bigrams
@@ -11,15 +16,27 @@ public class Helper {
         int n = input.length();
         String[] tmpStr = input.split("");
 
+        Boolean parity;
+
         ArrayList<String> result = new ArrayList<String>();
 
+        List<String> Xset = Stream.of(tmpStr).collect(Collectors.toList());
+
         // handling two similar chars
-        for (int i = 0; i < n; i++) {
-            if (tmpStr[i] == tmpStr[i + 1]) {
-                // TODO: - adding 'x' between two similar chars
+        for(int i = 1; i < Xset.size(); i++){
+            if(Xset.get(i).equals(Xset.get(i-1))){
+                Xset.add(i, "X");
             }
         }
+        if(Xset.size() % 2 != 0){
+            Xset.add("X");
+        }
+        for(int i = 0; i < Xset.size(); i++){
+            System.out.print(Xset.get(i) + " ");
+        }
 
+
+        
         // main loop
         for (int i = 0; i < n; i += 2) {
             // handling last char when it has no pair
